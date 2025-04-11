c51015e6db871fce117ad370a5d59730870a75d4
NetAcad DEVASC <changeme@example.com>
2025-04-11 18:06:34 +0000

    Arreglo Commit

--- datosjson.py
+++ datosjson.py
@@ -1,29 +1,26 @@
-# datosjson.py
+
 import json
 from datetime import datetime
 
-# Definir el nombre del archivo JSON
+
 nombre_archivo = 'myfile.json'
 
 try:
-    # Abrir el archivo JSON en modo lectura
+    
     with open(nombre_archivo, 'r') as json_file:
-        # Cargar el contenido del archivo JSON en la variable ourjson
+       
         ourjson = json.load(json_file)
 
-    # Verificar si la clave 'tokens' existe y es una lista
+    
     if 'tokens' in ourjson and isinstance(ourjson['tokens'], list):
-        # Iterar a través de cada token en la lista
+       
         for item in ourjson['tokens']:
             token_value = item.get('token')
             expiry_time_str = item.get('expiry_time')
 
             if token_value and expiry_time_str:
-                # Convertir la cadena de tiempo de expiración a un objeto datetime
                 expire_datetime = datetime.fromisoformat(expiry_time_str.replace('Z', '+00:00'))
-                # Obtener la fecha y hora actual (considerando la zona horaria local)
                 now = datetime.now()
-                # Calcular el tiempo restante
                 time_remaining = expire_datetime - now
 
                 print(f"Token: {token_value}")
